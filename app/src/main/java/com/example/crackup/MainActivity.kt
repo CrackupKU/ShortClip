27b2f8026abdbf26579adde87c33f91920dce9f1
Bhokin Watanapitak <zbeamz@hotmail.com>
2024-02-03 20:27:29 +0700

    Add buttom navigation bar

--- app/src/main/java/com/example/crackup/MainActivity.kt
+++ app/src/main/java/com/example/crackup/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.crackup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.crackup.databinding.ActivityMainBinding
+import com.example.crackup.util.UiUtil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavbar.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.bottom_menu_home -> {
+                    UiUtil.showToast(this,"Home")
+                }
+                R.id.bottom_menu_add_video->{
+                    UiUtil.showToast(this,"Add video")
+                    // Go to add video page
+                }
+                R.id.bottom_menu_profile->{
+                    UiUtil.showToast(this,"Profile")
+                    // Go to profile page
+                }
+            }
+            false
+        }
     }
 }
