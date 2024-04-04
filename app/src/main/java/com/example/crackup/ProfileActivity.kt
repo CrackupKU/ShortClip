d7a39d0990b5423f02973d306e03c3308738e181
Bhokin Watanapitak <zbeamz@hotmail.com>
2024-04-05 03:14:00 +0700

    Styling component

--- app/src/main/java/com/example/crackup/ProfileActivity.kt
+++ app/src/main/java/com/example/crackup/ProfileActivity.kt
@@ -3,9 +3,17 @@ package com.example.crackup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.crackup.api.RetrofitClient
 import com.example.crackup.databinding.ActivityProfileBinding
 import com.example.crackup.model.UserModel
 import com.google.firebase.auth.FirebaseAuth
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -36,4 +44,38 @@ class ProfileActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    private fun getProfileDataFromFirebase() {
+        val call: Call<UserModel> = RetrofitClient.apiService.getUser(profileUserId)
+        call.enqueue(object : Callback<UserModel> {
+            override fun onResponse(call: Call<UserModel>, response: Response<UserModel>) {
+                if (response.isSuccessful) {
+                    val user: UserModel? = response.body()
+                    if (user != null) {
+                        profileUserModel = user
+                        setUI()
+                    }
+                } else {
+                    Log.i("ProfileActivity", "Failed to user videos: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<UserModel>, t: Throwable) {
+                Log.i("ProfileActivity", "Network error: ${t.message}")
+            }
+        })
+
+    }
+
+    private fun setUI() {
+        profileUserModel.apply {
+            Glide.with(binding.profilePic).load(profilePic)
+                .apply(RequestOptions().placeholder(R.drawable.icon_profile_circle))
+                .into(binding.profilePic)
+            binding.profileUsername.text = "@" + username
+            binding.progressBar.visibility = View.INVISIBLE
+            binding.followingCount.text = followingList.size.toString()
+            binding.followerCount.text = followerList.size.toString()
+            // TODO: Fetch video by user HERE
+        }
+    }
 }
