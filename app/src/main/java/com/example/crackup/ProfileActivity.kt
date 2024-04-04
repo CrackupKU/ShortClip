4411bbba6a73c3595d204d1df32c7f75e0e2dace
Bhokin Watanapitak <zbeamz@hotmail.com>
2024-04-05 02:12:24 +0700

    UI of profile page

--- app/src/main/java/com/example/crackup/ProfileActivity.kt
+++ app/src/main/java/com/example/crackup/ProfileActivity.kt
@@ -1,18 +1,39 @@
 package com.example.crackup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.crackup.databinding.ActivityMainBinding
 import com.example.crackup.databinding.ActivityProfileBinding
-import com.example.crackup.util.UiUtil
+import com.example.crackup.model.UserModel
+import com.google.firebase.auth.FirebaseAuth
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProfileBinding
+    private lateinit var profileUserId: String
+    private lateinit var currentUserId: String
+    private lateinit var profileUserModel: UserModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        profileUserId = intent.getStringExtra("profile_user_id")!!
+        currentUserId = FirebaseAuth.getInstance().currentUser?.uid!!
+
+        if (profileUserId == currentUserId) {
+            binding.profileBtn.text = "Logout"
+            binding.profileBtn.setOnClickListener {
+                logout()
+            }
+        }
     }
+
+    private fun logout() {
+        FirebaseAuth.getInstance().signOut()
+        val intent = Intent(this, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
+    }
+
 }
