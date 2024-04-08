f4bf93a7968ec2cd353f3051d02e5da98a10e348
Bhokin Watanapitak <zbeamz@hotmail.com>
2024-04-08 16:24:16 +0700

    Interface of video player

--- app/src/main/java/com/example/crackup/adapter/VideoPagerAdapter.kt
+++ app/src/main/java/com/example/crackup/adapter/VideoPagerAdapter.kt
@@ -4,18 +4,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.crackup.R
 import com.example.crackup.databinding.VideoPlayerBinding
+import com.example.crackup.model.UserModel
 import com.example.crackup.model.reponse.VideosResponse
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class VideoPagerAdapter(private val shortVideos: List<VideosResponse>) :
     RecyclerView.Adapter<VideoPagerAdapter.VideoViewHolder>() {
 
+    private var isExpanded = false
+    private val MAX_CAPTION_LENGTH = 35
+
     inner class VideoViewHolder(private val binding: VideoPlayerBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bindVideo(shortVideo: VideosResponse) {
+            Firebase.firestore.collection("users")
+                .document(shortVideo.uploadBy)
+                .get().addOnSuccessListener {
+                    val userModel = it?.toObject(UserModel::class.java)
+                    userModel?.apply {
+                        binding.usernameView.text = username
+                        Glide.with(binding.profileIcon).load(profilePic)
+                            .circleCrop()
+                            .apply(
+                                RequestOptions().placeholder(R.drawable.icon_profile_circle)
+                            )
+                            .into(binding.profileIcon)
+
+                    }
+                }
+
+            binding.titleView.text = shortVideo.title
+            binding.progressBar.visibility = View.VISIBLE
+            val caption = shortVideo.caption
+            if (caption.length > MAX_CAPTION_LENGTH) {
+                val truncatedCaption = caption.substring(0, MAX_CAPTION_LENGTH).trim() + " ..."
+                binding.captionView.text = truncatedCaption
+            } else {
+                binding.captionView.text = caption
+            }
+
             binding.videoView.apply {
                 setVideoPath(shortVideo.videoUrl)
                 setOnPreparedListener {
+                    binding.progressBar.visibility = View.GONE
                     it.start()
                     it.isLooping = true
                 }
@@ -27,6 +63,20 @@ class VideoPagerAdapter(private val shortVideos: List<VideosResponse>) :
                     } else {
                         start()
                         binding.pauseIcon.visibility = View.GONE
+                    }
+                }
+            }
+            binding.captionView.apply {
+                setOnClickListener {
+                    if (caption.length > MAX_CAPTION_LENGTH) {
+                        if (isExpanded) {
+                            val truncatedCaption = caption.substring(0, MAX_CAPTION_LENGTH).trim() + " ..."
+                            binding.captionView.text = truncatedCaption
+
+                        } else {
+                            binding.captionView.text = caption
+                        }
+                        isExpanded = !isExpanded
                     }
                 }
             }
